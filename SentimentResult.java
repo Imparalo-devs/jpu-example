21365ca020cf88e23815425fd0f2f4052b792ee9
Imparalo-devs <sviluppo-interno@happywpress.com>
2026-04-10 19:54:39 +0200

    feat: Basic sentiment analyzer [vibe-code]

---
+++ SentimentResult.java
@@ -0,0 +1,17 @@
+public class SentimentResult {
+    private String sentiment;
+    private double confidence;
+
+    public SentimentResult(String sentiment, double confidence) {
+        this.sentiment = sentiment;
+        this.confidence = confidence;
+    }
+
+    public String getSentiment() {
+        return sentiment;
+    }
+
+    public double getConfidence() {
+        return confidence;
+    }
+}
