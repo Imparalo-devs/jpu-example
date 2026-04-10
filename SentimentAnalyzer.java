11a2b0e0cca2a77594c83acd7590909972de4654
Imparalo-devs <sviluppo-interno@happywpress.com>
2026-04-10 20:01:18 +0200

    feat: Basic sentiment analyzer [vibe-code]

---
+++ SentimentAnalyzer.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SentimentAnalyzer {
+    private ChatDatabase chatDatabase;
+    private SentimentIPU sentimentIPU;
+
+    public SentimentAnalyzer(ChatDatabase chatDatabase, SentimentIPU sentimentIPU) {
+        this.chatDatabase = chatDatabase;
+        this.sentimentIPU = sentimentIPU;
+    }
+
+    public void start() {
+        String currentChat;
+        while ((currentChat = chatDatabase.getChat()) != null) {
+            String currentSentiment = sentimentIPU.processSentiment(currentChat);
+            double resultConfidence = sentimentIPU.getResultConfidence();
+            System.out.println("Result Sentiment: " + currentSentiment);
+            System.out.println("Result Confidence: " + resultConfidence);
+        }
+    }
+}
