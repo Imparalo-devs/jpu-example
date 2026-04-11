a7400885d691262520e42eb79346a1b6ea2a144b
Imparalo-devs <sviluppo-interno@happywpress.com>
2026-04-11 10:41:50 +0200

    feat: Basic sentiment analyzer [vibe-code]

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        ChatDatabase chatDatabase = new ChatDatabase();
+        SentimentAnalyzer sentimentAnalyzer = new SentimentAnalyzer(chatDatabase);
+        sentimentAnalyzer.analyzeSentiments();
+    }
+}
