c54ab0b505ec82dd4b414f35ed0dd2ebc770ab32
Imparalo-devs <sviluppo-interno@happywpress.com>
2026-04-11 19:18:04 +0200

    feat: Basic sentiment analyzer [vibe-code]

---
+++ SentimentAnalyzer.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.concurrent.Future;
+import java.util.concurrent.ExecutionException;
+
+public class SentimentAnalyzer {
+    private ChatDatabase chatDatabase;
+    private SentimentIPU sentimentIPU;
+
+    public SentimentAnalyzer() {
+        this.chatDatabase = new ChatDatabase();
+        this.sentimentIPU = new SentimentIPU();
+    }
+
+    public void analyzeSentiments() throws InterruptedException, ExecutionException {
+        String currentChat = chatDatabase.getChat();
+        while (currentChat != null) {
+            sentimentIPU.writeInput("chatInput", currentChat);
+            Future<String> sentimentFuture = sentimentIPU.readOutput("sentiment");
+            Future<String> confidenceFuture = sentimentIPU.readOutput("confidence");
+            String resultSentiment = sentimentFuture.get();
+            String resultConfidence = confidenceFuture.get();
+            System.out.println("Result Sentiment: " + resultSentiment);
+            System.out.println("Result Confidence: " + resultConfidence);
+            currentChat = chatDatabase.getChat();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        SentimentAnalyzer sentimentAnalyzer = new SentimentAnalyzer();
+        sentimentAnalyzer.analyzeSentiments();
+    }
+}
