7765c12234e69c74556feb425a63df58b43dad3e
Imparalo-devs <sviluppo-interno@happywpress.com>
2026-04-13 18:03:49 +0200

    feat: Basic sentiment analyzer [vibe-code]

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        SentimentAnalyzer sentimentAnalyzer = new SentimentAnalyzer();
+        sentimentAnalyzer.analyzeSentiment();
+    }
+}
